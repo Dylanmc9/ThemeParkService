f5dd09afcab00f427cf69f9699dce60604304967
Dylan <dylanmcleer96@gmail.com>
2019-05-06 15:07:37 +0100

    SQL file updated

--- theme_parks.sql
+++ theme_parks.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 05, 2019 at 06:25 PM
+-- Generation Time: May 06, 2019 at 04:06 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -40,6 +40,30 @@ CREATE TABLE `tbl_parks` (
 INSERT INTO `tbl_parks` (`id`, `name`, `city`, `ticket_price`) VALUES
 (0, 'Universal', 'Orlando', 100),
 (0, 'Disneyland', 'California', 90);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(2000) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `created`, `modified`) VALUES
+(0, 'Dylan', 'McLeer', 'dylanmcleer@gmail.com', '$2y$10$9oZhJeS7JX41BDQRr5hz2.jydqnDfNdn8EwoqkCIvn/Rg9Fo.I1NK', '2019-05-06 11:33:53', '2019-05-06 12:54:24'),
+(0, 'Jim', 'Halpert', 'jimhalpert@mail.com', '$2y$10$lqbZCdnoSoN4pzXx5FyEmeY.hhF5PAsODmc3FSUQaon7R79/syt9i', '2019-05-06 14:17:08', '2019-05-06 13:17:08');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
